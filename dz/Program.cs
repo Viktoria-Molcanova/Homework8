194dbb268750be3e72188056a350f10b981a981b
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-29 22:10:15 +0700

    56

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,5 +1,74 @@
-﻿
-Console.WriteLine("Введите число ");
-int a = Convert.ToInt32(Console.ReadLine());
-a = (a / 10) % 10;
-Console.WriteLine(a);
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите размер массива");
+int A = InputNumbers("Введите A: ");
+int B = InputNumbers("Введите B: ");
+int diapazon = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[A, B];
+CreateArray(array);
+WriteArray(array);
+
+//Метод создания массива
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(diapazon);
+    }
+  }
+}
+
+//Метод печати массива
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+//Нахождение строки с меньшей суммой элементов
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+//Вывод результата
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой из {sumLine} элементов ");
+
+
